4be3b68ca88fb24ab67c20de13edbb1374d69b63
Sisir8642 <baburambista01@gmail.com>
2025-09-15 17:08:29 +0545

    fix: images with the secure protocol

--- backend/books/views.py
+++ backend/books/views.py
@@ -22,12 +22,21 @@ class BookAPIView(APIView):
             return Response({"error": "Book not found"}, status=404)
 
         book = data[key]
+        cover_url = book.get("cover", {}).get("medium")
+
+        
+        if cover_url:
+            if cover_url.startswith("//"):
+                cover_url = "https:" + cover_url
+        else:
+            cover_url = None  
+
         result = {
             "title": book.get("title"),
             "authors": [author["name"] for author in book.get("authors", [])],
             "publish_date": book.get("publish_date"),
             "publishers": [p["name"] for p in book.get("publishers", [])],
-            "cover": book.get("cover", {}).get("medium")
+            "cover": cover_url
         }
 
         return Response(result)
