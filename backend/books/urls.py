e4dfc48d7a01c5f644172ad2e9b8c45c848dbbef
Sisir8642 <baburambista01@gmail.com>
2025-09-15 16:56:49 +0545

    fix:the urls

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookAPIView
 
 urlpatterns = [
-    path('api/books/', BookAPIView.as_view(), name='get_book_by_isbn'),
+    path('books/', BookAPIView.as_view(), name='get_book_by_isbn'),
 ]
